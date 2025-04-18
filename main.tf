d8e849238df5e7f4f61c65c914385cf6761b46ab
Adity Ravanaa <adityasainathp@gmail.com>
2025-04-18 12:52:07 -0500

    main commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+resource "aws_instance" "example" {
+  ami           = "ami-05572e392e80aee89" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Test"
+  }
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "mys3bucket-test-aditya-1995"
+
+  tags = {
+    Name        = "MyBucket_aditya_sai_123456"
+    Environment = "Dev"
+  }
+}
